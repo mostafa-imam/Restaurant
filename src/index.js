ea42a7b2f7d0125d0f21489bd9d8f6a19ef9f976
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2025-08-10 18:40:26 +0300

    feat: Implement tab-switching logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import loadHomePage from './home';
 import loadMenuPage from './menu';
 import loadContactPage from './contact';
 
-// loadHomePage();
-// loadMenuPage();
-loadContactPage();
+const content = document.querySelector("#content");
+const home = document.querySelector("button.home");
+const menu = document.querySelector("button.menu");
+const contact = document.querySelector("button.contact");
+
+home.addEventListener("click", () => {
+    content.textContent = '';
+    loadHomePage();
+});
+
+menu.addEventListener("click", () => {
+    content.textContent = '';
+    loadMenuPage();
+});
+
+contact.addEventListener("click", () => {
+    content.textContent = '';
+    loadContactPage();
+});
+
+loadHomePage();
