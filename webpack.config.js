fead81f522bdc5097287b985db72ab98abcb6a70
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2025-08-10 14:20:46 +0300

    chore(deps): Configure webpack for html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: "asset",
             },
+            {
+              test: /\.html$/i,
+              loader: "html-loader",
+            },
         ],
     },
 };
